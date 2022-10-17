e433573e3147b14415016886e4d69444f5169e15
prabhaakaar <115984159+prabhaakaar@users.noreply.github.com>
2022-10-17 14:39:39 +0530

    Create raise.cpp

---
+++ raise.cpp
@@ -0,0 +1,44 @@
+/*  C++ Program to raise any number X to power N  */
+
+#include<iostream>
+#include<math.h>   //for pow() function
+using namespace std;
+
+int main()
+{
+
+    int x,n,result;
+
+    cout<<"Enter value of X :: ";
+    cin>>x;
+    cout<<"\nEnter value of N :: ";
+    cin>>n;
+
+    result=pow(x,n);
+
+    cout<<"\nThe Power of Number [ "<<x<<" ^ "<<n<<" ] = "<<result<<"\n";
+
+    return 0;
+}
+/*  C++ Program to raise any number X to power N  */
+
+#include<iostream>
+#include<math.h>   //for pow() function
+using namespace std;
+
+int main()
+{
+
+    int x,n,result;
+
+    cout<<"Enter value of X :: ";
+    cin>>x;
+    cout<<"\nEnter value of N :: ";
+    cin>>n;
+
+    result=pow(x,n);
+
+    cout<<"\nThe Power of Number [ "<<x<<" ^ "<<n<<" ] = "<<result<<"\n";
+
+    return 0;
+}
